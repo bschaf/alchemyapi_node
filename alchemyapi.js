8547e902650f70feee21fa2b7968fe54e03ad877
Brandon Schafer <brandon@Brandons-MacBook-Pro.local>
2015-08-07 17:24:30 -0600

    read API key from .env

--- alchemyapi.js
+++ alchemyapi.js
@@ -57,29 +57,29 @@ if (require.main === module) {
 
 
 
-function AlchemyAPI() {
+function AlchemyAPI(opts) {
 
-	//Load the key from api_key.txt
-	try {
-		key = fs.readFileSync(__dirname + '/api_key.txt').toString().trim();
-	}
-	catch(err) {
-		//Create the file
-		fs.openSync(__dirname + '/api_key.txt', 'w');
-		console.log('API key not detected in api_key.txt, please run: node alchemyapi.js YOUR_KEY_HERE');
-		console.log('If you do not have a key, register for one at: http://www.alchemyapi.com/api/register.html');
-		process.exit(1);
-	}
+	// //Load the key from api_key.txt
+	// try {
+	// 	key = fs.readFileSync(__dirname + '/api_key.txt').toString().trim();
+	// }
+	// catch(err) {
+	// 	//Create the file
+	// 	fs.openSync(__dirname + '/api_key.txt', 'w');
+	// 	console.log('API key not detected in api_key.txt, please run: node alchemyapi.js YOUR_KEY_HERE');
+	// 	console.log('If you do not have a key, register for one at: http://www.alchemyapi.com/api/register.html');
+	// 	process.exit(1);
+	// }
 	
-	//Make sure the key formating looks good
-	if (key.length != 40) {
-		console.log('The API key in api_key.txt does not appear to be valid. Make sure to run: node alchemyapi.js YOUR_KEY_HERE');
-		console.log('If you do not have a key, register for one at: http://www.alchemyapi.com/api/register.html');
-		process.exit(1);
-	}
+	// //Make sure the key formating looks good
+	// if (key.length != 40) {
+	// 	console.log('The API key in api_key.txt does not appear to be valid. Make sure to run: node alchemyapi.js YOUR_KEY_HERE');
+	// 	console.log('If you do not have a key, register for one at: http://www.alchemyapi.com/api/register.html');
+	// 	process.exit(1);
+	// }
 
 	//Set the key
-	this.apikey = key;
+	this.apikey = opts.api_key;
 
 
 	/**
